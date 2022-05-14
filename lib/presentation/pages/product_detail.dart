37ac3119c8da96daf6bf7ad5f592ccb75588c75a
cosmin.george8 <459623@student.saxion.nl>
2022-05-14 12:01:36 +0200

    - added product detail

---
+++ lib/presentation/pages/product_detail.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../data/entities/product.dart';
+
+class ProductDetailPage extends StatefulWidget {
+  final Product product;
+  const ProductDetailPage({Key? key, required this.product}) : super(key: key);
+
+  @override
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
